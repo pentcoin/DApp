3c093116696061350844ef6f6a792cb7c0a37c4a
xenshiba <134064946+xenshiba@users.noreply.github.com>
2023-06-22 01:43:57 +0100

    Delete consts directory

--- consts/contractAddresses.ts
+++
@@ -1,6 +0,0 @@
-export const nftDropContractAddress =
-  "0xec02441Aecd9551F4cD1d6410792CE70f4E4C7dB";
-export const tokenContractAddress =
-  "0xCc328212884f37E6972D4799CC2be2efcaa91B95";
-export const stakingContractAddress =
-  "0x5ce3F4C4DA55f61a20E4045f0bef4633399e62c2";
