bafd79b1b8fd59e293f509d7816d09701b52a149
xenshiba <134064946+xenshiba@users.noreply.github.com>
2023-06-22 01:46:47 +0100

    Add files via upload

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0x9643e783124bA4A38E9D9964172D43e545c17382";
+export const tokenContractAddress =
+  "0x8688Ee261fE911c9d05F54536A2538E87F0e0c80";
+export const stakingContractAddress =
+  "0x6b1F5597EC0C47F75E5Fca09E6c2baBbD685942B";
